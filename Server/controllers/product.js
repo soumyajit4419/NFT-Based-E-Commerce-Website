836615460553592859d6d34f16e6f13d8f98c7db
Rahul Kumar Patro <rahulkpatro@gmail.com>
2022-07-10 19:32:15 +0530

    added products details and basic routes

---
+++ Server/controllers/product.js
@@ -0,0 +1,82 @@
+const product_model = require("../models/product.js");
+const { customAlphabet } = require("nanoid");
+
+exports.add_products = async (req, res) => {
+  try {
+    let product_data = req.body.product_data;
+
+    await Promise.all(
+      product_data.map((eachproduct) => {
+        let product_id = customAlphabet("1234567890abcdef", 10)();
+        eachproduct.product_id = product_id;
+        eachproduct.product_serial_number =
+          eachproduct.product_brand.substring(0, 2) +
+          Math.random().toString(30).slice(2);
+      })
+    );
+
+    await product_model.insertMany(product_data);
+
+    res.status(200).json({
+      message: "All Products Added Successfully"
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Some error occured",
+      error: `${error.name}, ${error.message}, ${error.stack}`
+    });
+  }
+};
+
+exports.get_all_products = async (req, res) => {
+  try {
+    var product_data = await product_model.find({});
+
+    res.status(200).json({
+      message: "All Product details fetched Successfully",
+      products: product_data,
+      total_products: product_data.length
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Some error occured",
+      error: `${error.name}, ${error.message}, ${error.stack}`
+    });
+  }
+};
+
+exports.get_all_categories = async (req, res) => {
+  try {
+    var categories = await product_model.distinct("category", {});
+
+    res.status(200).json({
+      message: "All Product categories fetched Successfully",
+      all_category: categories,
+      total: categories.length
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Some error occured",
+      error: `${error.name}, ${error.message}, ${error.stack}`
+    });
+  }
+};
+
+exports.each_category_products = async (req, res) => {
+  try {
+    var category = req.query.category;
+
+    var products = await product_model.find({ category: category });
+
+    res.status(200).json({
+      message: "Products of particular category fetched sucessfully",
+      products: products,
+      total_products: products.length
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Some error occured",
+      error: `${error.name}, ${error.message}, ${error.stack}`
+    });
+  }
+};
--- Server/util/database.js
+++ Server/util/database.js
@@ -1,24 +1,21 @@
 const mongoose = require("mongoose");
-// const MongoClient = mongodb.MongoClient;
 
-const mongoConnect = (callback) => {
+const mongo_uri =
+  "mongodb+srv://codedrift:rahul158@cluster0.rmrec.mongodb.net/flipkart_grid_nft";
+
+const mongoconnectdb = (callback) => {
   mongoose
-    .connect(
-      process.env.MONGODB_URL ||
-        "mongodb+srv://superadmin:EDMK3lPSqIXxdAQH@louddatabase.kruit.mongodb.net/loudDatabase?retryWrites=true&w=majority",
-
-      {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useUnifiedTopology: true,
-      }
-    )
-    .then((res) => {
-      callback(res);
+    .connect(mongo_uri, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true
     })
-    .catch((err) => {
-      console.log(err, "error this is incommming");
+    .then(() => {
+      console.log("Connected with the MongoDB Atlas Database");
+    })
+    .catch((error) => {
+      console.log(error, "Not Connected with the Database ");
     });
 };
 
-module.exports = mongoConnect;
+module.exports = mongoconnectdb;

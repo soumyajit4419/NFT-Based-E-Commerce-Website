3c37fb5f91ea3c4164f7b3e6b0d043d6ca6dd488
Rahul Kumar Patro <rahulkpatro@gmail.com>
2022-07-16 20:29:56 +0530

    feat: added payment page and faq page and send msg functionality added

---
+++ Server/controllers/sendemail.js
@@ -0,0 +1,31 @@
+const nodemailer = require("nodemailer");
+const config = require("./../config");
+
+const sendEmail = (subject, htmlMessage, email) => {
+    let transport = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.email.username,
+            pass: process.env.email.password
+        },
+        secure: true,
+        pool: true
+    });
+
+    const message = {
+        from: "Flipkart NFT Team Team <rahulkpatro@gmail.com>",
+        to: email,
+        subject: subject, // Subject line
+        html: htmlMessage
+    };
+    transport.sendMail(message, function (err, info) {
+        if (err) {
+            console.log(err);
+            return;
+        } else {
+            console.log("Email Sent");
+        }
+    });
+}
+
+module.exports = sendEmail;
--- Server/routes/userRoutes.js
+++ Server/routes/userRoutes.js
@@ -2,8 +2,9 @@ const express = require("express");
 const app = express();
 const product = require("../controllers/product.js");
 const user = require("../controllers/user.js");
-
+const nft = require("../controllers/nft.js");
 const verify = require("../middlewares/verify");
+const sendmsg = require("../controllers/sendmsg");
 
 // user routes
 app.route("/register").post(user.register);
@@ -13,6 +14,9 @@ app.route("/login").post(user.login);
 app.route("/user").get(user.get_user);
 
 app.route("/valid_user").get(verify.valid_user);
+
+//send message (15 credits left so dont use this api unless required)
+// app.route("/sendmsg").post(verify.verify, sendmsg.sendmsg);
 
 // product routes
 app.route("/add_product").post(product.add_products);
@@ -24,5 +28,7 @@ app.route("/all_category").get(product.get_all_categories);
 app.route("/category_products").get(product.each_category_products);
 
 app.route("/product").get(product.each_product);
+
+app.route("/mint").get(nft.nft);
 
 module.exports = app;

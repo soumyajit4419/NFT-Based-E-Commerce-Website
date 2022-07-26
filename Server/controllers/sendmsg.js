83dd3da2f48b2f28bf4ac6d6d8f928ff997c7608
Rahul Kumar Patro <rahulkpatro@gmail.com>
2022-07-26 23:18:07 +0530

    uncommented send message

--- Server/controllers/sendmsg.js
+++ Server/controllers/sendmsg.js
@@ -1,29 +1,31 @@
-// const accountid = process.env.TWILIO_ACCOUNT_SID;
+const accountid = process.env.TWILIO_ACCOUNT_SID;
 
-// const authtoken = process.env.TWILIO_AUTH_TOKEN;
+const authtoken = process.env.TWILIO_AUTH_TOKEN;
 
-// const client = require("twilio")(accountid, authtoken);
+const client = require("twilio")(accountid, authtoken);
 
-// exports.sendmsg = async (req, res) => {
-//   try {
-//     let twilioNum = process.env.TWILIO_PHONE_NUMBER;
+exports.sendmsg = async (req, res) => {
+  try {
+    let twilioNum = process.env.TWILIO_PHONE_NUMBER;
 
-//     const phone = req.body.phone;
+    const phone = req.body.phone;
 
-//     await client.messages.create({
-//       body: `Testing message sms`,
-//       from: twilioNum,
-//       to: phone
-//     });
+    const text = req.body.text;
 
-//     res.status(200).json({ message: "Message sent successfully" });
-//   } catch (error) {
-//     res.status(500).json({
-//       message: "Some error occured while sending the message",
-//       error: `${error.name}, ${error.message}, ${error.stack}`
-//     });
-//   }
-// };
+    await client.messages.create({
+      body: text,
+      from: twilioNum,
+      to: phone
+    });
+
+    res.status(200).json({ message: "Message sent successfully" });
+  } catch (error) {
+    res.status(500).json({
+      message: "Some error occured while sending the message",
+      error: `${error.name}, ${error.message}, ${error.stack}`
+    });
+  }
+};
 
 // const fast2sms = require("fast-two-sms");
 // const { getResponsesTag } = require("swagger-autogen/src/swagger-tags");

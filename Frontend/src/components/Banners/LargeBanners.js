cd0609246d09849e6b7d1ac76160e963402ecd45
soumyajit4419 <ashish454570@gmail.com>
2022-07-29 12:02:10 +0530

    added new image

--- Frontend/src/components/Banners/LargeBanners.js
+++ Frontend/src/components/Banners/LargeBanners.js
@@ -8,8 +8,8 @@ import {
 
 function LargeBanners() {
   const itemsUrls = [
-    "https://k.nooncdn.com/ads/banner-1008x1008/en_dk_uae-hero-01%20(22).1657872603.9944868.png",
-    "https://k.nooncdn.com/cms/pages/20220715/14b0cd4f1646398325b2ce7aba448419/en_dk_uae-hero-01-9DAYS.jpg",
+    "https://k.nooncdn.com/cms/pages/20220724/8cdd2318ac00462e41a92f4da2f423ee/en_dk_uae-hero-BEAUTY.jpg",
+    "https://k.nooncdn.com/cms/pages/20220725/a9cf976dabcad0337875a37063780f30/en_noon-hero-BGS-01.png",
     "https://k.nooncdn.com/cms/pages/20220715/206a0db1b7625953f7afb536ca2c0743/en_mb-noon-hero-mms-01.png",
   ];
 
